98179454b310f4f426917954fa141e66bb93b67b
Corey <>
2022-09-14 08:45:08 -0500

    Include tools for rendering notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosectionlabel",
+    "myst_nb"
 ]
 
 autosummary_generate = True
@@ -27,6 +28,8 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+# --- Options for rendering notebooks ----------------
+nb_exectution_mode='off'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
